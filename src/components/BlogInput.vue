8d5cb180c11deaa03e417bb68242531524cfbf93
moon <anstnqls554@gmail.com>
2022-08-26 10:51:10 +0900

    목록 보이기, 목록 삭제하기

--- src/components/BlogInput.vue
+++ src/components/BlogInput.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="input-wrap shadow">
     <input type="text" v-model="newItem" class="input-box" maxlength="30" @keyup.enter="addItem">
-    <!-- <button v-on:click="addItem">추가</button> -->
     <span @click="addItem" class="add-bt">
       <i class="fas fa-plus add-bt-icon"></i>
     </span>
@@ -17,8 +16,12 @@ export default {
       let temp = newItem.value;
       // 앞쪽 뒷쪽 공백 제거
       temp = temp.trim();
+      // 추후 업데이트 예정(정규표현식-문자열체크 문법)
+      //  공백 '' 공백 '' 공백 앞,뒷자리 공백을 없애야함
       if(temp !== '') {
         //localStorage.setItem(키, 값)
+        // 값은 추후에 json 형태로 만들어서 저장
+        // JSON.stringify(오브젝트)
         //localStorage.setItem(키, json 문자열로 저장)
         // json 저장 문자열
         /*
--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -1,13 +1,65 @@
 <template>
   <h1>List</h1>
+  <div class="list-wrap">
+<ul>
+  <!-- v-for in :key -->
+  <li v-for="(item, index) in memoItemArr" :key="index" class="shadow"> {{item}} 
+    <span class="remove-bt" @click="removeMemo(item, index)">
+      <i class="fas fa-trash-alt"></i>
+    </span>
+  </li>
+</ul>
+  </div>
 </template>
 
 <script>
+import {ref, reactive} from 'vue';
 export default {
+setup(){
+  // localstorage의 목록을 가지고온다.
+  const total = ref(0);
+  total.value = localStorage.length;
+  // 키네임을 저장하는 배열
+  const memoItemArr = reactive([])
 
+  if(localStorage.length > 0){
+    for(let i = 0; i < total.value; i++){
+      // 배열에 요소를 밀어넣는다.
+      memoItemArr.push(localStorage.key(i))
+    }
+  }
+  const removeMemo = (item, index)=>{
+    // localStorage 에서 key를 통해서 지운다.
+    localStorage.removeItem(item);
+    // 배열(memoItempArr) 에서도 지운다.
+    memoItemArr.splice(index, 1)
+  }
+  return{
+    memoItemArr,removeMemo
+  }
+}
 }
 </script>
 
-<style>
+<style scoped>
+li {
+  position: relative;
+  display: flex;
+  min-height: 50px;
+  line-height: 50px;
+  margin: 10px 0;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 0 20px;
+}
 
+.remove-bt{
+  margin-left: auto;
+  cursor: pointer;
+  color: hotpink;
+}
+
+span i{
+  font-size: 20px;
+}
 </style>
